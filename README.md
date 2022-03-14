9d178a24cff187bb76687dce947c2da783aed379
Carlos Garcia <alu0101208268@ull.edu.es>
2022-03-14 22:57:39 +0000

    completing README

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Car-Path-finding
+## Description
+This proyect is a simulation of a car that find the best path from the start to the end using differents heuristics.
+### Heuristics
+* [Manhattan distance](https://en.wikipedia.org/wiki/Taxicab_geometry)
+* [Euclidean distance](https://en.wikipedia.org/wiki/Euclidean_distance#:~:text=In%20mathematics%2C%20the%20Euclidean%20distance,being%20called%20the%20Pythagorean%20distance.) 
+## How to use it
+First of all we have to install the grafics library called sfml using this command  ```sudo apt-get install libsfml-dev```
+
+Then to execute it you have to write ```make run``` and it will appear the menu of the program.
--- include/entorno.hpp
+++ include/entorno.hpp
@@ -17,71 +17,71 @@ typedef std::vector<std::vector<Casilla>> Matriz_casillas;
 
 // Clase que actua como espacio entorno donde
 class Entorno {
-   public:
-    Entorno(int, int);
-    Entorno(std::string);
-    virtual ~Entorno();
-    // Getters
-    std::vector<std::vector<Casilla>> get_entorno();
-    int get_filas() { return filas_; }
-    int get_columnas() { return columnas_; }
-    std::pair<int, int> get_inicio() { return inicio_; }
-    std::pair<int, int> get_final() { return final_; }
-    bool get_calcular();
-    bool get_reset();
+ public:
+  Entorno(int, int);
+  Entorno(std::string);
+  virtual ~Entorno();
+  // Getters
+  std::vector<std::vector<Casilla>> get_entorno();
+  int get_filas() { return filas_; }
+  int get_columnas() { return columnas_; }
+  std::pair<int, int> get_inicio() { return inicio_; }
+  std::pair<int, int> get_final() { return final_; }
+  bool get_calcular();
+  bool get_reset();
 
-    // setter
-    void set_matriz(Matriz_casillas entorno);
-    void set_calcular(bool);
-    // Entorno sfml
-    void reset_entorno();
-    void clean();
+  // setter
+  void set_matriz(Matriz_casillas entorno);
+  void set_calcular(bool);
+  // Entorno sfml
+  void reset_entorno();
+  void clean();
 
-    void iniciar_entorno() {
-        ventana = new sf::RenderWindow(dimension, "Entorno", sf::Style::Titlebar | sf::Style::Close);
-        ventana->setFramerateLimit(60);
-    }
-    //Inicilizacion
+  void iniciar_entorno() {
+    ventana = new sf::RenderWindow(dimension, "Entorno", sf::Style::Titlebar | sf::Style::Close);
+    ventana->setFramerateLimit(60);
+  }
+  // Inicilizacion
 
-    // Renderizacion
-    void renderizarEntorno();
+  // Renderizacion
+  void renderizarEntorno();
 
-    void renderizar();
+  void renderizar();
 
-    //Actualizacion
-    void actualizarPosMouse();
-    void actualizarEvento();
-    void actualizarCasillas();
+  // Actualizacion
+  void actualizarPosMouse();
+  void actualizarEvento();
+  void actualizarCasillas();
 
-    void actualizar();
+  void actualizar();
 
-    const bool ejecutando() const;
+  const bool ejecutando() const;
 
-    // Inicializacion inicio, final y obstaculos
-    void inicializacion_manual();
-    void inicializacion_auto(bool);  // true = manual, false = consola
-    std::ostream& write(std::ostream&) const;
+  // Inicializacion inicio, final y obstaculos
+  void inicializacion_manual();
+  void inicializacion_auto(bool);  // true = manual, false = consola
+  std::ostream& write(std::ostream&) const;
 
-   private:
-    // Estructura
-    std::vector<std::vector<Casilla>> matriz_entorno;
-    int filas_;
-    int columnas_;
-    std::pair<int, int> inicio_;
-    std::pair<int, int> final_;
-    int porcentaje_;
-    bool calcular_;
-    bool reset_;
-    bool clean_;
+ private:
+  // Estructura
+  std::vector<std::vector<Casilla>> matriz_entorno;
+  int filas_;
+  int columnas_;
+  std::pair<int, int> inicio_;
+  std::pair<int, int> final_;
+  int porcentaje_;
+  bool calcular_;
+  bool reset_;
+  bool clean_;
 
-    // Entorno grafico sfml
-    sf::RenderWindow* ventana;
-    std::vector<std::vector<sf::RectangleShape>> matriz_sf;
-    bool mouse_presionado;
-    sf::Event evento;
-    sf::Vector2i posicion_mouse;
-    sf::Vector2f VistaPosMouse;
-    sf::VideoMode dimension;
+  // Entorno grafico sfml
+  sf::RenderWindow* ventana;
+  std::vector<std::vector<sf::RectangleShape>> matriz_sf;
+  bool mouse_presionado;
+  sf::Event evento;
+  sf::Vector2i posicion_mouse;
+  sf::Vector2f VistaPosMouse;
+  sf::VideoMode dimension;
 };
 
 #endif
